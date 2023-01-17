07f0e6e32af8d2ce92780a26dcaba368ef300810
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-01-17 14:23:46 -0300

    Feat base of Web Project of Habit
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <div className="bg-zinc-900 w-16 h-16 text-white rounded m-2 p-4 text-center flex items-center justify-center">
+      <h1>Olá Habits</h1>
+    </div>
+  );
+}
