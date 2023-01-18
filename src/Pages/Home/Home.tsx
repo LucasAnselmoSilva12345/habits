7719f2e1bd4f82f6a1a8b112a71171761b7de6f9
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-01-18 15:28:48 -0300

    Feat Summary Table Component and Home page
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { Header } from '../../components/Header/Header';
+import { SummaryTable } from '../../components/SummaryTable/SummaryTable';
 import '../../styles/global.css';
 
 export function Home() {
@@ -6,6 +7,7 @@ export function Home() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
+        <SummaryTable />
       </div>
     </div>
   );
