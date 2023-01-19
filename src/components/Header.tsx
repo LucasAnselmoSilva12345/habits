2e87711e336c83f7ac53f0832695de0c5eb72c5a
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-01-19 15:23:02 -0300

    Feat Habit and New Register Screen's Feat React Navigation Package
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,9 @@ export function Header() {
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
 
-        <Text className="text-white ml-3 font-semibold text-base"></Text>
+        <Text className="text-white ml-3 font-semibold text-base">
+          Novo hábito
+        </Text>
       </TouchableOpacity>
     </View>
   );
