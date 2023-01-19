a66a867055d466c62c593a1e8f42fcf7212d8da7
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-01-19 16:12:00 -0300

    Feat New Habit Interface
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,7 +1,15 @@
 import { useState } from 'react';
-import { ScrollView, View, Text, TextInput } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import { BackButton } from '../components/BackButton';
 import { Checkbox } from '../components/Checkbox';
+import colors from 'tailwindcss/colors';
 
 const availableWeekDays = [
   'Domingo',
@@ -28,7 +36,10 @@ export function New() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl">
@@ -39,7 +50,11 @@ export function New() {
           Qual seu comprometimento?
         </Text>
 
-        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-500" />
+        <TextInput
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-500"
+          placeholder="Ir na academia, tomar 30m de sol, etc..."
+          placeholderTextColor={colors.zinc[400]}
+        />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
@@ -53,6 +68,17 @@ export function New() {
             onPress={() => handleToggleWeekDays(index)}
           />
         ))}
+
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
