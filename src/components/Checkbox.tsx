6314f3f62f953261306ffa9afb19c4e43862e1a4
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-01-21 15:43:36 -0300

    Feat animation of checkbox
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -6,6 +6,7 @@ import {
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
+import Animated, { FlipInXDown, FlipOutXDown } from 'react-native-reanimated';
 
 interface CheckboxProps extends TouchableOpacityProps {
   title: string;
@@ -20,9 +21,13 @@ export function Checkbox({ checked = false, title, ...rest }: CheckboxProps) {
       {...rest}
     >
       {checked ? (
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={FlipInXDown}
+          exiting={FlipOutXDown}
+        >
           <Feather name="check" size={20} color={colors.white} />
-        </View>
+        </Animated.View>
       ) : (
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       )}
